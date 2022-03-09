3e0ff3f313d57751ad21bfda70f239cd255ef3b5
Guy Royse <guy@guyroyse.com>
2022-03-09 11:29:28 -0500

    bind empty CRUD routes to /person

--- server.js
+++ server.js
@@ -4,9 +4,15 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import YAML from 'yamljs'
 
+/* import routers */
+import { router as personRouter } from './routers/person-router.js'
+
 /* create an express app and use JSON */
 const app = new express()
 app.use(express.json())
+
+/* bring in some routers */
+app.use('/person', personRouter)
 
 /* set up swagger in the root */
 const swaggerDocument = YAML.load('api.yaml')
