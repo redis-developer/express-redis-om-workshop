d009680c05259e87e5f0d3444863d1c3a29aba8e
Guy Royse <guy@guyroyse.com>
2022-03-09 12:35:33 -0500

    fill out /all route

--- routers/search-router.js
+++ routers/search-router.js
@@ -4,4 +4,7 @@ import { personRepository } from '../om/person.js'
 
 export const router = Router()
 
-router.get('/all', async (req, res) => {})
+router.get('/all', async (req, res) => {
+  const persons = await personRepository.search().return.all()
+  res.send(persons)
+})
