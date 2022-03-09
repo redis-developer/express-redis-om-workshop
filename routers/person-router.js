758aaf6e47fef40b87d5e13ac015e7b354f2b85e
Guy Royse <guy@guyroyse.com>
2022-03-09 11:24:54 -0500

    create empty routes for CRUD operations

--- routers/person-router.js
+++ routers/person-router.js
@@ -3,3 +3,15 @@ import { Router } from 'express'
 import { personRepository } from '../om/person.js'
 
 export const router = Router()
+
+// CREATE
+router.put('/', async (req, res) => {})
+
+// READ
+router.get('/:id', async (req, res) => {})
+
+// UPDATE
+router.post('/:id', async (req, res) => {})
+
+// DELETE
+router.delete('/:id', async (req, res) => {})
