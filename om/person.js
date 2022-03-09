0585486c829a782cba537ddf48cf5ed0c778e451
Guy Royse <guy@guyroyse.com>
2022-03-09 11:20:29 -0500

    define our person schema

--- om/person.js
+++ om/person.js
@@ -3,3 +3,14 @@ import { Entity, Schema } from 'redis-om'
 import client from './client.js'
 
 class Person extends Entity {}
+
+const personSchema = new Schema(Person, {
+  firstName: { type: 'string' },
+  lastName: { type: 'string' },
+  age: { type: 'number' },
+  verified: { type: 'boolean' },
+  location: { type: 'point' },
+  locationUpdated: { type: 'date' },
+  skills: { type: 'string[]' },
+  personalStatement: { type: 'text' }
+})
