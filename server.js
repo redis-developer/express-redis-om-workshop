cce2eb8d484e58ed17cb277861239e7f31bd6294
Guy Royse <guy@guyroyse.com>
2022-03-09 13:14:05 -0500

    bind location router to /person

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ import YAML from 'yamljs'
 /* import routers */
 import { router as personRouter } from './routers/person-router.js'
 import { router as searchRouter } from './routers/search-router.js'
+import { router as locationRouter } from './routers/location-router.js'
 
 /* create an express app and use JSON */
 const app = new express()
 app.use(express.json())
 
 /* bring in some routers */
-app.use('/person', personRouter)
+app.use('/person', personRouter, locationRouter)
 app.use('/persons', searchRouter)
 
 /* set up swagger in the root */
