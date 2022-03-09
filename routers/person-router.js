45c2bd9350d2fb42d23fe378c6419db3e32a537f
Guy Royse <guy@guyroyse.com>
2022-03-09 11:54:31 -0500

    fill out update person route

--- routers/person-router.js
+++ routers/person-router.js
@@ -17,7 +17,23 @@ router.get('/:id', async (req, res) => {
 })
 
 // UPDATE
-router.post('/:id', async (req, res) => {})
+router.post('/:id', async (req, res) => {
+
+  const person = await personRepository.fetch(req.params.id)
+
+  person.firstName = req.body.firstName ?? null
+  person.lastName = req.body.lastName ?? null
+  person.age = req.body.age ?? null
+  person.verified = req.body.verified ?? null
+  person.location = req.body.location ?? null
+  person.locationUpdated = req.body.locationUpdated ?? null
+  person.skills = req.body.skills ?? null
+  person.personalStatement = req.body.personalStatement ?? null
+
+  await personRepository.save(person)
+
+  res.send(person)
+})
 
 // DELETE
 router.delete('/:id', async (req, res) => {})
