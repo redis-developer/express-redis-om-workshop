cd40a7273d8fa50e9a1a4a7f2a681975d63b087f
Guy Royse <guy@guyroyse.com>
2022-03-09 12:42:06 -0500

    add a route that searches by a string

--- routers/search-router.js
+++ routers/search-router.js
@@ -8,3 +8,10 @@ router.get('/all', async (req, res) => {
   const persons = await personRepository.search().return.all()
   res.send(persons)
 })
+
+router.get('/by-last-name/:lastName', async (req, res) => {
+  const lastName = req.params.lastName
+  const persons = await personRepository.search()
+    .where('lastName').equals(lastName).return.all()
+  res.send(persons)
+})
